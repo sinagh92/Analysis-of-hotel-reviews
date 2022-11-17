3ba672bb871629326c12d8b5cee94ed5837dda2d
sinagh92 <sinagh92@gmail.com>
2022-11-16 16:08:18 -0800

    Code review is done. Positive and negative review classes are added.

---
+++ Modules.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Reviews:
+    def __init__(self):
+        pass
+        
+    
+    words = []
+    all_sentences = []
+    all_reviews = []
+    clusters_words = []
+    all_sentences= []
+    cluster_score =  []
+    clusters= []
+    clusters_words = []
+    clusters_supp = []
+    cluster_sentence = []
+    all_reviews = []
+    
